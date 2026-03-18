c5e3a8688da9cf5a62cda7f1cf90ab2a0a0091c0
Victor Manuel Gonzalez Nahuat <victor@MacBook-Air-de-Victor.local>
2026-03-18 14:37:46 -0600

    arr

--- database.py
+++ database.py
@@ -54,7 +54,6 @@ def obtenerProductosPorNombre(nombre):
     try:
         conn = get_connection()
         cursor = conn.cursor()
-        #Se buscan los productos en INARMA01
         cursor.execute("""
             SELECT id_codigo, id_descripcion 
             FROM INARMA01 
@@ -62,37 +61,12 @@ def obtenerProductosPorNombre(nombre):
             ORDER BY id_descripcion ASC
             LIMIT 100
         """, (nombre + '%',))
-        #Productos INARMA01
-        INARMA01 = cursor.fetchall()
+        productos_encontrados = cursor.fetchall()
 
-        #Se buscan los productos de INARMA02
-        cursor.execute("""
-            SELECT id_codigo, id_descripcion
-            FROM INARMA02
-            WHERE id_descripcion LIKE %s
-            ORDER BY id_descripcion ASC
-            LIMIT 100
-        """, (nombre + '%',))
-
-        #Productos INARMA02
-        INARMA02 = cursor.fetchall()
-        #Si no hay productos en ninguno retorna None
-        if not INARMA01 and not INARMA02:
+        if not productos_encontrados:
             return None
 
-        #Se crean dos listas (para INARMA01 e INARMA02) con diccionarios para cada producto
-        productos_INARMA01 = []
-        productos_INARMA02 = []
-        if INARMA01:
-            productos_INARMA01 = [{"codigo": codigo, "nombre": descripcion} for codigo, descripcion in INARMA01]
-
-        if INARMA02:
-            productos_INARMA02 = [{"codigo": codigo, "nombre": descripcion} for codigo, descripcion in INARMA02]
-
-
-        #Se crea una nueva lista compuesta por las listas de productos de ambas tablas
-        productos_vinculo = [productos_INARMA01, productos_INARMA02]
-        return productos_vinculo
+        return [{"codigo": codigo, "nombre": descripcion} for codigo, descripcion in productos_encontrados]
     finally:
         if conn:
             conn.close()
@@ -111,100 +85,47 @@ def obtenerProductosPorCodigo(codigo):
         """, (codigo,))
         tearma01 = cursor.fetchone()
 
-        cursor.execute("""
-            SELECT id_codigo, id_descripcion, id_grupo, id_maximo, id_minimo, id_lista1, id_provee
-            FROM INARMA02
-            WHERE id_codigo = %s
-        """, (codigo,))
-        tearma02 = cursor.fetchone()
-
-        if not tearma01 and not tearma02:
+        if not tearma01:
             return None
 
-        producto01 = None
-        if tearma01:
-            (codigo01, nombre01, grupo01, maximo01, minimo01, precio_lista01, id_proveedor01) = tearma01
+        (codigo01, nombre01, grupo01, maximo01, minimo01, precio_lista01, id_proveedor01) = tearma01
 
-            cursor.execute("""
-                SELECT dt_sadoinicial, dt_entradas, dt_salidas, dt_ultimo_costo, dt_ultima_venta, dt_ultima_compra
-                FROM INARAR01
-                WHERE dt_codigo = %s
-            """, (codigo,))
-            inarar01 = cursor.fetchone()
+        cursor.execute("""
+            SELECT dt_sadoinicial, dt_entradas, dt_salidas, dt_ultimo_costo, dt_ultima_venta, dt_ultima_compra
+            FROM INARAR01
+            WHERE dt_codigo = %s
+        """, (codigo,))
+        inarar01 = cursor.fetchone()
 
-            existencia01 = None
-            ultimo_costo01 = None
-            ultima_venta01 = None
-            if inarar01:
-                (saldo_inicial, entradas, salidas, ultimo_costo01, ultima_venta01, ultima_compra01) = inarar01
-                existencia01 = (saldo_inicial or 0) + (entradas or 0) - (salidas or 0)
+        existencia01 = None
+        ultimo_costo01 = None
+        ultima_venta01 = None
+        ultima_compra01 = None
+        if inarar01:
+            (saldo_inicial, entradas, salidas, ultimo_costo01, ultima_venta01, ultima_compra01) = inarar01
+            existencia01 = (saldo_inicial or 0) + (entradas or 0) - (salidas or 0)
 
-            cursor.execute("""
-                SELECT dt_cliente
-                FROM PRARMA01
-                WHERE dt_codigoc = %s
-            """, (id_proveedor01,))
-            proveedor01 = cursor.fetchone()
-            nombre_proveedor01 = proveedor01[0] if proveedor01 else "Desconocido"
+        cursor.execute("""
+            SELECT dt_cliente
+            FROM PRARMA01
+            WHERE dt_codigoc = %s
+        """, (id_proveedor01,))
+        proveedor01 = cursor.fetchone()
+        nombre_proveedor01 = proveedor01[0] if proveedor01 else "Desconocido"
 
-            producto01 = {
-                "codigo": codigo01,
-                "nombre": nombre01,
-                "grupo": grupo01,
-                "maximo": maximo01,
-                "minimo": minimo01,
-                "precio": precio_lista01,
-                "existencia": existencia01,
-                "ultimo_costo": ultimo_costo01,
-                "ultima_venta": ultima_venta01,
-                "ultima_compra": ultima_compra01,
-                "proveedor": nombre_proveedor01
-            }
-
-        producto02 = None
-        if tearma02:
-            (codigo02, nombre02, grupo02, maximo02, minimo02, precio_lista02, id_proveedor02) = tearma02
-
-            cursor.execute("""
-                SELECT dt_sadoinicial, dt_entradas, dt_salidas, dt_ultimo_costo, dt_ultima_venta, dt_ultima_compra
-                FROM INARAR02
-                WHERE dt_codigo = %s
-            """, (codigo,))
-            inarar02 = cursor.fetchone()
-
-            existencia02 = None
-            ultimo_costo02 = None
-            ultima_venta02 = None
-            if inarar02:
-                (saldo_inicial, entradas, salidas, ultimo_costo02, ultima_venta02, ultima_compra02) = inarar02
-                existencia02 = (saldo_inicial or 0) + (entradas or 0) - (salidas or 0)
-
-            cursor.execute("""
-                SELECT dt_cliente
-                FROM PRARMA02
-                WHERE dt_codigoc = %s
-            """, (id_proveedor02,))
-            proveedor02 = cursor.fetchone()
-            nombre_proveedor02 = proveedor02[0] if proveedor02 else "Desconocido"
-
-            producto02 = {
-                "codigo": codigo02,
-                "nombre": nombre02,
-                "grupo": grupo02,
-                "maximo": maximo02,
-                "minimo": minimo02,
-                "precio": precio_lista02,
-                "existencia": existencia02,
-                "ultimo_costo": ultimo_costo02,
-                "ultima_venta": ultima_venta02,
-                "ultima_compra": ultima_compra02,
-                "proveedor": nombre_proveedor02
-            }
-
-        return [
-            producto01 if producto01 else {},
-            producto02 if producto02 else {}
-        ]
+        return {
+            "codigo": codigo01,
+            "nombre": nombre01,
+            "grupo": grupo01,
+            "maximo": maximo01,
+            "minimo": minimo01,
+            "precio": precio_lista01,
+            "existencia": existencia01,
+            "ultimo_costo": ultimo_costo01,
+            "ultima_venta": ultima_venta01,
+            "ultima_compra": ultima_compra01,
+            "proveedor": nombre_proveedor01
+        }
 
     finally:
         if conn:
